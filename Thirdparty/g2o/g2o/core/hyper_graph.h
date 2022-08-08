a09320bb217f894faa27622ce38b1898e77d7d2c
Sherman Chen <schen@simviu.com>
2022-08-08 12:15:03 +0800

    [g2o]: build fix by remote "tr1"

--- Thirdparty/g2o/g2o/core/hyper_graph.h
+++ Thirdparty/g2o/g2o/core/hyper_graph.h
@@ -38,7 +38,9 @@
 #ifdef _MSC_VER
 #include <unordered_map>
 #else
-#include <tr1/unordered_map>
+//#include <tr1/unordered_map>
+// most compiler now support unordered_map
+#include <unordered_map>
 #endif
 
 
@@ -90,7 +92,7 @@ namespace g2o {
       typedef std::set<Edge*>                           EdgeSet;
       typedef std::set<Vertex*>                         VertexSet;
 
-      typedef std::tr1::unordered_map<int, Vertex*>     VertexIDMap;
+      typedef std::unordered_map<int, Vertex*>     VertexIDMap;
       typedef std::vector<Vertex*>                      VertexContainer;
 
       //! abstract Vertex, your types must derive from that one
